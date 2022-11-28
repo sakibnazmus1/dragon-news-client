81fb698a4d97d57dbf2c2f2836b78db83be1b734
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-11-28 12:22:11 +0600

    first commit

---
+++ src/__step.js
@@ -0,0 +1,11 @@
+// BASIC PROJECT SETUP 
+// 1. create firebase app
+// 2. Get firebase configaration inside firebase.config.js
+// Make sure to export app from firebase config
+// 3. create a react app
+// 4. install firebase and react-douter-dom
+
+
+// ROUTER SETUP
+// 1.
+
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC27PoU2FWEhnjV5hBYO-WMOIVI-cvt71M",
+    authDomain: "dragon-news-b1983.firebaseapp.com",
+    projectId: "dragon-news-b1983",
+    storageBucket: "dragon-news-b1983.appspot.com",
+    messagingSenderId: "1048265539198",
+    appId: "1:1048265539198:web:b4d6dea7e5184d588da22f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
