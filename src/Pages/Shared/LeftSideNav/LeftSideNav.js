b57252cf1a4ac40949ffc0f8073441477e8e6683
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-11-28 21:21:06 +0600

    different catagories added

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
 
 const LeftSideNav = () => {
     const [catagories, setCatagories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/news-catagories')
+            .then(res => res.json())
+            .then(data => setCatagories(data));
+    }, [])
+
     return (
         <div>
-            <h4>All catagories</h4>
+            <h4>All catagories{catagories.length}</h4>
+            <div>
+                {
+                    catagories.map(catagory => <p key={catagory.id}>
+                        <Link to={`/catagory/${catagory.id}`}>{catagory.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
