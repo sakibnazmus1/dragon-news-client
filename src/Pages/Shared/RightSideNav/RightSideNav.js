cf7f80f75750be8f74b872b8eb8f29671f257d8d
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-11-29 19:22:50 +0600

    added react icons

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub } from 'react-icons/fa';
+
 
 const RightSideNav = () => {
     return (
         <div>
             <ButtonGroup vertical>
-                <Button variant="outline-primary">Login With Google</Button>
-                <Button variant="outline-dark">Login With Github</Button>
+                <Button variant="outline-primary"><FaGoogle></FaGoogle> Login With Google</Button>
+                <Button variant="outline-dark"><FaGithub></FaGithub> Login With Github</Button>
             </ButtonGroup>
         </div>
     );
