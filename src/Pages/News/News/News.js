bd9a7d1c31f703b45ab3049c04c1217646398aa3
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-12-07 11:51:11 +0600

    category added

--- src/Pages/News/News/News.js
+++ src/Pages/News/News/News.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
 
 const News = () => {
+    const news = useLoaderData();
+    const { title, image_url, details } = news
     return (
-        <div>
-            <h2>This is news</h2>
-        </div>
+        <Card>
+            <Card.Img variant="top" src={image_url} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {details}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
     );
 };
 
