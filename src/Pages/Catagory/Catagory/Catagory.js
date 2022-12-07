3e813f9fc18640dc32c3f2a0b09018b7c7d76cd8
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-12-07 10:51:38 +0600

    category section added

--- src/Pages/Catagory/Catagory/Catagory.js
+++ src/Pages/Catagory/Catagory/Catagory.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsSumarryCard from '../../Shared/NewsSummaryCard/NewsSumarryCard';
 
 const Catagory = () => {
-    const news = useLoaderData();
+    const catagoryNews = useLoaderData();
     return (
         <div>
-            <h2>This is catagory has news:{news.length}</h2>
+            <h2>This is catagory has news:{catagoryNews.length}</h2>
+            {
+                catagoryNews.map(news => <NewsSumarryCard
+                    key={news._id}
+                    news={news}
+                ></NewsSumarryCard>)
+            }
         </div>
     );
 };
