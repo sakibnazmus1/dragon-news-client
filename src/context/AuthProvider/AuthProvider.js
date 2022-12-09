453bc0e1cd840a08dba132483457ff9e8e5bcc5a
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-12-09 10:51:51 +0600

    added outside api

--- src/context/AuthProvider/AuthProvider.js
+++ src/context/AuthProvider/AuthProvider.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { createContext } from 'react';
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signInWithPopup } from "firebase/auth";
 import app from '../../firebase/firebase.config';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const AuthContext = createContext();
 const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
 
-    const user = { displayName: 'Batash Ali' }
+    const [user, setUser] = useState(null);
 
 
     const providerLogin = (provider) => {
         return signInWithPopup(auth, provider);
     }
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('user inside state changed', currentUser)
+            setUser(currentUser)
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
 
     const authInfo = { user, providerLogin }
 
