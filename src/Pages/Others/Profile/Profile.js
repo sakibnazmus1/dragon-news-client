56121315dbb52193129bbbb4c2381cfc94bee020
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-12-22 15:31:47 +0600

    edited profile changed

--- src/Pages/Others/Profile/Profile.js
+++ src/Pages/Others/Profile/Profile.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+import { useRef } from 'react';
 import { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -6,8 +8,20 @@ import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
 const Profile = () => {
     const { user } = useContext(AuthContext)
+    const [name, setName] = useState(user.displayName)
+    const photoURLRef = useRef(user.photoURL)
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(photoURLRef.current.value);
+    }
+
+    const handleNameChanged = event => {
+        setName(event.target.value);
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control readOnly defaultValue={user?.email} type="email" placeholder="Enter email" />
@@ -15,11 +29,11 @@ const Profile = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Your Name</Form.Label>
-                <Form.Control defaultValue={user?.displayName} type="text" placeholder="Name" />
+                <Form.Control onChange={handleNameChanged} defaultValue={name} type="text" placeholder="Name" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Photo URL</Form.Label>
-                <Form.Control defaultValue={user?.photoURL} type="text" placeholder="PhotoURL" />
+                <Form.Control ref={photoURLRef} defaultValue={user?.photoURL} type="text" placeholder="PhotoURL" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
